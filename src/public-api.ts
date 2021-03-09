ada291d0e4e163ee5c9faf374c6849a7077bb664
jgu7man <jgu7man@gmail.com>
2021-03-08 23:02:36 -0600

    first version library

---
+++ src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of alert
+ */
+
+export * from './lib/alert.service';
+export * from './lib/alert.module';
+export * from './lib/alerta-popup/alerta-popup.component'
+export * from './lib/error-popup/error-popup.component'
+export * from './lib/alerts.model'
